40069bedcf8b1994428cc723338deec69c717040
Shathish2004 <shathishkumaran07@gmail.com>
2025-12-19 23:12:30 +0530

    Made some changes in the start command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import mongoose from "mongoose";
-import { startApolloServer } from "./src/app";
+import { startApolloServer } from "./app";
 
 const mongoUri = process.env.MONGO_URI;
 
